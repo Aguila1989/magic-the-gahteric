0b9b8a6b17fe88462149fcab3105fcb19bf96317
kdeme <k.demeulenaere@hotmail.com>
2024-05-02 15:10:07 +0200

    made dto and mapping

---
+++ Howest.MagicCards.Shared/DTO/CardDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.Shared.DTO
+{
+    public record CardDTO
+    {
+        public string Id { get; init; }
+        public string Name { get; init; }
+        public string Type { get; init; }
+        public string RarityCode { get; init; }
+        public string ImageUrl { get; init; }
+    }
+}
---
+++ Howest.MagicCards.Shared/DTO/CardDetailDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.Shared.DTO
+{
+    public record CardDetailDTO
+    {
+        public string Id { get; init; }
+        public string Name { get; init; }
+        public string Type { get; init; }
+        public string RarityCode { get; init; }
+        public string ImageUrl { get; init; }
+        public string Text { get; init; }
+        public string Flavor {  get; init; }
+        public int Power {  get; init; }
+        public int Toughness { get; init; }
+    }
+}
