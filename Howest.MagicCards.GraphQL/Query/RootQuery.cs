816330a18bbc164d27f268d50266135a33de7e96
kdeme <k.demeulenaere@hotmail.com>
2024-05-07 15:24:40 +0200

    made my first query to fetch cards

--- Howest.MagicCards.GraphQL/Query/RootQuery.cs
+++ Howest.MagicCards.GraphQL/Query/RootQuery.cs
@@ -1,8 +1,18 @@
-﻿using GraphQL.Types;
+﻿using GraphQL;
+using GraphQL.Types;
+using Howest.MagicCards.DAL.Repositories;
+using Howest.MagicCards.GraphQL.Types;
 
-namespace Howest.MagicCards.GraphQL.Query
+namespace Howest.MagicCards.GraphQL.GraphQL.Query
 {
     public class RootQuery : ObjectGraphType
     {
+        public RootQuery(ICardRepository cardRepo, IArtistRepository artistRepo)
+        {
+            Field<ListGraphType<CardType>>(
+                "cards",
+                resolve: context => (cardRepo.GetAllCards()).ToList()
+            );
+        }
     }
 }
