f9ee7b8744e1e019fab39f8f673f10728b5af858
kdeme <k.demeulenaere@hotmail.com>
2024-05-07 13:19:47 +0200

    kinda fixed my filter on types

--- Howest.MagicCards.Shared/Extensions/CardExtensions.cs
+++ Howest.MagicCards.Shared/Extensions/CardExtensions.cs
@@ -9,9 +9,9 @@ namespace Howest.MagicCards.Shared.Extensions
             IQueryable<Card> res = cards;
 
             res = res.Where(c => set == null || c.SetCode == set);
-            res = res.Where(c => type == null || c.Type.Contains(type));
-            res = res.Where(c => name == null || c.Name.Contains(name));
-            res = res.Where(c => text == null || c.Text.Contains(text));
+            res = res.Where(c => type == null || c.CardTypes.Any(ct => ct.Type.Name.ToLower() == type.ToLower()));
+            res = res.Where(c => name == null || c.Name.ToLower().Contains(name.ToLower()));
+            res = res.Where(c => text == null || c.Text.ToLower().Contains(text.ToLower()));
             res = res.Where(c => artist == null || c.Artist == artist);
             res = res.Where(c => rarity == null || c.RarityCode == rarity);
 
