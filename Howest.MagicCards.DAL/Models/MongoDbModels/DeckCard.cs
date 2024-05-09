db452d85cf85b2fb36d725456d6ed222eff438b1
kdeme <k.demeulenaere@hotmail.com>
2024-05-09 02:50:39 +0200

    added models for minimal Api

---
+++ Howest.MagicCards.DAL/Models/MongoDbModels/DeckCard.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Howest.MagicCards.DAL.Models.MongoDbModels
+{
+    public class DeckCard
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? DeckCardId { get; set; }
+
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Quantity { get; set; }
+    }
+}
