9d026ec8227afef642139b0abfdaf88772c3b2b5
kdeme <k.demeulenaere@hotmail.com>
2024-05-09 02:58:09 +0200

    added mongoDB builder to program and database to appsettings

--- Howest.MagicCards.MinimalAPI/Program.cs
+++ Howest.MagicCards.MinimalAPI/Program.cs
@@ -1,9 +1,13 @@
+using BookStoreApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<MongoDBSettings>(
+    builder.Configuration.GetSection("MtgDatabase"));
 
 var app = builder.Build();
 
